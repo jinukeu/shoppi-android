9348646332c6eb58c35c001d25f58d71fe032dab
koownij <koownij@kakao.com>
2022-03-14 20:56:27 +0900

    Section 5

--- app/src/main/java/com/kunize/shoppi_android/MainActivity.kt
+++ app/src/main/java/com/kunize/shoppi_android/MainActivity.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_proudct_detail)
+        setContentView(R.layout.activity_main)
     }
 }
